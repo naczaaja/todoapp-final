32ff7415e1d73229d3866d3032216b5081801545
Chirayu Chomsri <chirayu.chomsri@gmail.com>
2022-07-23 22:32:33 +0700

    add Log Interceptor

--- frontend/src/app/interceptors/rest.interceptor.ts
+++ frontend/src/app/interceptors/rest.interceptor.ts
@@ -3,20 +3,30 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpResponse
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class RestInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor() { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const url = `${environment.baseURL}${request.url}`
     const urlReq = request.clone({ url })
 
-    return next.handle(urlReq);
+    console.log("REQ: RestInterceptor");
+
+    return next.handle(urlReq).pipe(
+      tap(event => {
+        if (event instanceof HttpResponse) {
+
+        }
+      })
+    );
   }
 }
