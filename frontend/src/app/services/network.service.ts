630d19de4cb4799821721282172d3f15ee3db443
Chirayu Chomsri <chirayu.chomsri@gmail.com>
2022-08-01 16:39:25 +0700

    addtodo-new

--- frontend/src/app/services/network.service.ts
+++ frontend/src/app/services/network.service.ts
@@ -38,6 +38,7 @@ export class NetworkService {
       headers: new HttpHeaders()
         .set('Authorization',  `${localStorage.getItem('token')}`)
     }
+    addTodo.message += "-new"
     return this.httpClient.post<Todo>(`todos/create`,addTodo,headers)
   }
 }
